dab2dcf2d04c3e749f30fae8885efdb5f6ddc2e4
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2024-03-21 16:19:44 +0300

    Css side is done !

--- TeamGenerator/src/App.jsx
+++ TeamGenerator/src/App.jsx
@@ -1,39 +1,43 @@
-import { useState } from 'react'
-import Team from './Team'
-import './App.css'
-
-
+import { useState } from "react";
+import Team from "./Team";
+import "./App.css";
 
 function App() {
-
-
   //verilen arr içerisinden ilgili stringi döner..
-  function getRandomTeams(){
-    const teamsArray = ['Fenerbahce','Galatasaray','Besiktas','Bursa','Karagümrük','Sivas','Trabzonspor'];
-    return teamsArray[Math.floor(Math.random()*teamsArray.length)]
+  function getRandomTeams() {
+    const teamsArray = [
+      "Fenerbahce",
+      "Galatasaray",
+      "Besiktas",
+      "Bursa",
+      "Karagümrük",
+      "Sivas",
+      "Trabzonspor",
+    ];
+    return teamsArray[Math.floor(Math.random() * teamsArray.length)];
   }
 
   //teamsArray değiskenine önce bos arr ata.
-  const [teamsArray, setTeams] = useState([])
+  const [teamsArray, setTeams] = useState([]);
 
   //click tetiklenince içerisindeki elemanları tut ve yeni elemanı ata. setstate yap.
   const handleClick = () => {
-    setTeams([...teamsArray,getRandomTeams()])
-  }
+    setTeams([...teamsArray, getRandomTeams()]);
+  };
+
+
+  const teamsList=teamsArray.map((team, index) => {
+    return <Team key={index} teamName={team} />;
+  })
 
   return (
-    <>
-    <button onClick={handleClick}>Takım Ekle</button>
+    <div className="App">
+      <button className="appButton" onClick={handleClick}>
+        Takım Ekle
+      </button>
 
-
-    {// her bir teamsArr dolas ve her birine team değiskenine atayarak dön.
-      teamsArray.map((team,index) => {
-        return <Team key={index} teamName={team}/>
-      })
-    }
-
-    
-    </>
+      <div className="teamsList">{teamsList}</div>
+    </div>
   );
 }
 
--- TeamGenerator/src/Team.jsx
+++ TeamGenerator/src/Team.jsx
@@ -1,31 +1,30 @@
-import Fenerbahce from './images/fb.png';
-import Galatasaray from './images/gs.png';
-import Besiktas from './images/bjk.png';
-import Bursa from './images/bursa.png';
-import Karagümrük from './images/karagümrük.png';
-import Sivas from './images/sivas.png';
-import Trabzonspor from './images/ts.png';
-
+import Fenerbahce from "./images/fb.png";
+import Galatasaray from "./images/gs.png";
+import Besiktas from "./images/bjk.png";
+import Bursa from "./images/bursa.png";
+import Karagümrük from "./images/karagümrük.png";
+import Sivas from "./images/sivas.png";
+import Trabzonspor from "./images/ts.png";
+import './Team.css';
 
 const teamsMap = {
-    Fenerbahce:Fenerbahce,
-    Galatasaray:Galatasaray,
-    Besiktas:Besiktas,
-    Bursa:Bursa,
-    Karagümrük:Karagümrük,
-    Sivas:Sivas,
-    Trabzonspor:Trabzonspor
+  Fenerbahce: Fenerbahce,
+  Galatasaray: Galatasaray,
+  Besiktas: Besiktas,
+  Bursa: Bursa,
+  Karagümrük: Karagümrük,
+  Sivas: Sivas,
+  Trabzonspor: Trabzonspor,
+};
 
-}
-
-
-function Team({teamName}) {
-
-    return (
-        <>
-        <img src={teamsMap[teamName]}  />
-        </>
-    )
+function Team({ teamName }) {
+  return (
+    <div className="teamsDiv">
+      <div >
+        <img className="teams" src={teamsMap[teamName]} />
+      </div>
+    </div>
+  );
 }
 
 export default Team;
